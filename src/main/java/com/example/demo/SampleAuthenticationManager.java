f7573b1e76e466fa31a43acc4816f8138f74f69b
Santosh Chavva <santosh_chavva@epam.com>
2019-02-21 15:50:20 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/SampleAuthenticationManager.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SampleAuthenticationManager implements AuthenticationManager {
+    static final List<GrantedAuthority> AUTHORITIES = new ArrayList<GrantedAuthority>();
+
+    static {
+        AUTHORITIES.add(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        if (authentication.getName().equals(authentication.getCredentials())) {
+            return new UsernamePasswordAuthenticationToken(authentication.getName(),
+                    authentication.getCredentials(), AUTHORITIES);
+        }
+        throw new BadCredentialsException("Bad Credentials");
+    }
+}
